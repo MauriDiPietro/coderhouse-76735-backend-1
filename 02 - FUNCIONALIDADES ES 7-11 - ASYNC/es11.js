7e0e6a31012fb4312cdedf3f902c0004d9f44eeb
MauriDiPietro <dipietro.jm@gmail.com>
2025-12-15 22:33:36 -0300

    clase02 in progress

---
+++ 02 - FUNCIONALIDADES ES 7-11 - ASYNC/es11.js
@@ -0,0 +1,8 @@
+/* --------------------------------- nullish -------------------------------- */
+
+//valores en falso
+//0, null, undefined, false, NaN, ''
+
+let altura = 0
+
+console.log('Altura', altura ?? 100);
