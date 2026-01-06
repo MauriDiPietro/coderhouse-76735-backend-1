d6c42e626b04e6016f92ae2e0d42569613418eba
MauriDiPietro <dipietro.jm@gmail.com>
2026-01-05 22:31:16 -0300

    clase03 ok

--- 03 - MODULOS NATIVOS Y DE TERCEROS/04-fs-bcrypt-async/index.js
+++ 03 - MODULOS NATIVOS Y DE TERCEROS/04-fs-bcrypt-async/index.js
@@ -6,28 +6,32 @@ class UserManager {
     this.path = path;
   }
 
-  getUsers() {
-    if(!fs.existsSync(this.path)) return [];
-    const data = fs.readFileSync(this.path, "utf-8");
-    return JSON.parse(data);
+  async getUsers() {
+    try {
+      if(!fs.existsSync(this.path)) return [];
+      const data = await fs.promises.readFile(this.path, "utf-8");
+      return JSON.parse(data);
+    } catch (error) {
+      console.error("Error reading users:", error);
+    }
   }
 
-  register(obj) {
+  async register(obj) {
     try {
-      const users = this.getUsers();
+      const users = await this.getUsers();
       const user = { ...obj };
       user.password = bcrypt.hashSync(user.password, 10);
       users.push(user);
-      fs.writeFileSync(this.path, JSON.stringify(users));
+      await fs.promises.writeFile(this.path, JSON.stringify(users));
       return user;
     } catch (error) {
       console.error("Error creating user:", error);
     }
   }
 
-  login(email, password) {
+  async login(email, password) {
     try {
-      const users = this.getUsers();
+      const users = await this.getUsers();
       const user = users.find((u) => u.email === email);
       if (!user) throw new Error("Invalid credentials");
       const isValidPass = bcrypt.compareSync(password, user.password);
@@ -55,8 +59,8 @@ const user2 = {
     password: "5678",
 }
 
-userManager.register(user1);
-userManager.register(user2);
+await userManager.register(user1);
+await userManager.register(user2);
 
 // console.log(userManager.getUsers());
-console.log(userManager.login('juan@mail.com', '12345'));
+console.log(await userManager.login('juan@mail.com', '12345'));
